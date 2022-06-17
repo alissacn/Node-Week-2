d1c5bd6c6460bcdbe9bc284f40cab8b7e867578c
alissa <alissanishihara@gmail.com>
2022-06-17 18:25:27 +0200

    finish exercises

---
+++ messages.js
@@ -0,0 +1,15 @@
+class Message {
+    id
+    name
+    text
+    constructor(id, name, text) {
+        this.id = id;
+        this.from = from;
+        this.name = name;
+    }
+
+}
+
+module.exports = Message
+
+
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const { send } = require("express/lib/response");
+
 
 const app = express();
 
 app.use(cors());
+
+//for parsing app
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const welcomeMessage = {
   id: 0,
@@ -11,15 +17,85 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
+
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+let messages = [welcomeMessage];
+let availableId = 1;
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000")
+
+//messages 
+app.get("/messages", function (request, response) {
+  response.send(messages);
+});
+
+
+//post messages
+app.post("/messages", function (request, response) {
+  const {from, text} = request.body;
+  const newMessage = {from, text};
+  newMessage.id = availableId++;
+
+  if (!newMessage.from || !newMessage.text) {
+    response.sendStatus(400);
+    return;
+  };
+  
+  messages.push(newMessage);
+  response.sendStatus(201);
+});
+
+
+//search latest messages
+app.get("/latest", (request, response) => {
+  response.send(messages.slice(-10).reverse());
+
+});
+
+
+//search messages -okk
+app.get("/search", (request, response) => {
+  const textFilter = request.query.text;
+  const result = messages.filter(message => message.text.includes(textFilter));
+  
+  if (!result.length) {
+    response.sendStatus(404);
+    return;
+  }
+ 
+  response.send(result);
+});
+
+
+//search a message by id
+app.get("/messages/:messageId", (request, response) => {
+  const filterId = request.params.messageId;
+  const results = messages.filter(message => message.id == filterId);
+
+  if(!results) {
+    response.sendStatus(404);
+    return;
+  }
+ 
+  response.send(results);
+});
+
+
+//delete a message by id 
+app.delete("/messages/:id", (req, res) => {
+
+  const { id } = req.params;
+  messages = messages.filter(message => message.id != id);
+  
+  res.send(messages);
+});
+
+
+app.listen(3001, () => {
+   console.log("Listening on port 3001")
   });
